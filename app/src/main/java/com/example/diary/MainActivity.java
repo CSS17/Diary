ec7364aeb6c1ed409df57d25e974c83512e80c4e
CSS17 <ofucer99@gmail.com>
2022-11-01 19:13:05 +0300

    Animated Splash Screen added

--- app/src/main/java/com/example/diary/MainActivity.java
+++ app/src/main/java/com/example/diary/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.diary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        CountDownTimer waitTimer;
+        waitTimer = new CountDownTimer(2000, 300) {
+
+            public void onTick(long millisUntilFinished) {
+                //called every 300 milliseconds, which could be used to
+                //send messages or some other action
+            }
+
+            public void onFinish() {
+                //After 60000 milliseconds (60 sec) finish current
+                //if you would like to execute something when time finishes
+                Intent intent=new Intent(getApplicationContext(),Home.class);
+                startActivity(intent);
+                finish();
+            }
+        }.start();
+
+
+
+
     }
+
 }
